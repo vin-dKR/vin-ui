729f689e7a50de841b5d7e4845429a5c4aa95e44
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-04-05 22:54:52 +0530

    2nd bento done

--- components/ui/bento/SecondGrid.tsx
+++ components/ui/bento/SecondGrid.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import MotionRays from "./MotionRays"
 
 const SecondGrid = () => {
     return (
@@ -10,7 +9,6 @@ const SecondGrid = () => {
             bg-linear-to-br from-purple-400/5 via-white/5 to-black/30 backdrop-blur-lg 
             border border-white/10 border-2 shadow-lg
         ">
-            <MotionRays />
             <Image
                 src="/bento/motion-card-bg.svg"
                 alt="motion-bg"
@@ -18,15 +16,27 @@ const SecondGrid = () => {
                 height={200}
                 className="w-full contrast-100"
             />
-
             <Image
-                src="/spirals-curve.svg"
-                alt="spiral"
-                width={100}
-                height={100}
-                className="absolute w-64 rotate-180 right-0 bottom-0"
+                src="/bento/speedometer.svg"
+                alt="motion-bg"
+                width={200}
+                height={200}
+                className="absolute scale-120 left-[140px] top-[110px]"
             />
-
+            <div className="absolute left-[160px] top-[135px] h-40 w-40 flex items-center justify-center">
+                <Image
+                    src="/bento/speed-arrow.svg"
+                    alt="speedometer-arrow"
+                    width={200}
+                    height={200}
+                    className="
+                        h-40 w-40
+                        origin-center
+                        transition-transform duration-1000 ease-out -rotate-60
+                        group-hover:rotate-[150deg] group-hover:duration-[2000ms] group-hover:ease-in
+                    "
+                />
+            </div>
             <div className="absolute bottom-0 mx-6 my-3 md:my-10">
                 <h1 className="text-2xl">Motions</h1>
                 <p className="text-sm text-gray-900/50 dark:text-gray-400">120fps animations</p>
